5089889b231e8beb4b32b50e3ef1e8e8f6b71ba8
Bat-Erdene <batka.officials@gmail.com>
2026-07-29 13:09:43 +0800

    fix: standardize relation widget names (#8831)

--- frontend/libs/erxes-ui/src/types/UIConfig.ts
+++ frontend/libs/erxes-ui/src/types/UIConfig.ts
@@ -40,6 +40,7 @@ export type IUIConfig = {
     relationWidgets?: {
       name: string;
       icon?: React.ElementType;
+      label?: string;
     }[];
     customerDetailWidgets?: {
       name: string;

251f28e48257e1d430be053893b2a2d8d6ebdbb6
batmnkh2344 <batmnkh2344@gmail.com>
2026-08-10 13:31:02 +0800

    update

--- backend/plugins/blockadmin_api/src/modules/supplier/profile/graphql/resolvers/queries/supplier.ts
+++ backend/plugins/blockadmin_api/src/modules/supplier/profile/graphql/resolvers/queries/supplier.ts
@@ -1,9 +1,10 @@
 import { IContext } from '~/connectionResolvers';
 import { SupplierQueryParams } from '@/supplier/profile/@types/supplier';
-import { ICursorPaginateParams } from 'erxes-api-shared/core-types';
-import { markResolvers } from 'erxes-api-shared/utils';
+import { ICursorPaginateParams, IOffsetPaginateParams, Resolver } from 'erxes-api-shared/core-types';
+import { defaultPaginate } from 'erxes-api-shared/utils';
+import { generateFilter } from '../../../utils';
 
-export const supplierQueries = {
+export const supplierQueries: Record<string, Resolver> = {
   baSupplierDetail: async (
     _root: undefined,
     { _id }: { _id: string },
@@ -19,10 +20,30 @@ export const supplierQueries = {
   ) => {
     return models.Supplier.listSuppliers(params);
   },
+
+  cpBaSupplierDetail: async (
+    _root: undefined,
+    { _id }: { _id: string },
+    { models }: IContext,
+  ) => {
+    return models.Supplier.getSupplier(_id);
+  },
+
+  cpBaSuppliers: async (
+    _root: undefined,
+    params: SupplierQueryParams & IOffsetPaginateParams,
+    { models }: IContext,
+  ) => {
+    const filter = generateFilter(params);
+
+    return defaultPaginate(models.Supplier.find(filter), params);
+  },
 };
 
-markResolvers(supplierQueries, {
-  wrapperConfig: {
-    forClientPortal: true,
-  },
-})
+supplierQueries.cpBaSuppliers.wrapperConfig = {
+  forClientPortal: true,
+};
+
+supplierQueries.cpBaSupplierDetail.wrapperConfig = {
+  forClientPortal: true,
+};

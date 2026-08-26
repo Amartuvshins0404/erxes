b0eb6b96264c437e4d8e3c18e067fc994b0b6700
darjss <darjsavid@gmail.com>
2026-08-26 22:53:34 +0800

    fix(cf-os): inject command URL into UI bundle

--- frontend/plugins/cf_os_ui/rspack.config.ts
+++ frontend/plugins/cf_os_ui/rspack.config.ts
@@ -1,5 +1,6 @@
 import { composePlugins, withNx, withReact } from '@nx/rspack';
 import { withModuleFederation } from '@nx/rspack/module-federation';
+import { DefinePlugin } from '@rspack/core';
 
 import baseConfig from './module-federation.config';
 
@@ -7,4 +8,13 @@ export default composePlugins(
   withNx(),
   withReact(),
   withModuleFederation(baseConfig, { dts: false }),
+  (rspackConfig) => {
+    rspackConfig.plugins?.push(
+      new DefinePlugin({
+        'process.env.CF_OS_URL': JSON.stringify(process.env.CF_OS_URL),
+      }),
+    );
+
+    return rspackConfig;
+  },
 );
